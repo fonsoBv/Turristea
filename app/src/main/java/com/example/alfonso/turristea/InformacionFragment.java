5ea6da27f65dbafb300afe1e26b8e3f6b0729c00
fonsoBv <31110577+fonsoBv@users.noreply.github.com>
2018-05-01 18:23:03 -0600

    vistas

---
+++ app/src/main/java/com/example/alfonso/turristea/InformacionFragment.java
@@ -0,0 +1,23 @@
+package com.example.alfonso.turristea;
+
+import android.content.Context;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class InformacionFragment extends Fragment {
+
+    public InformacionFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_informacion, container, false);
+    }
+}
---
+++ app/src/main/java/com/example/alfonso/turristea/Turristea.java
@@ -0,0 +1,85 @@
+package com.example.alfonso.turristea;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.design.widget.NavigationView;
+import android.support.v4.app.Fragment;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
+
+public class Turristea extends AppCompatActivity
+        implements NavigationView.OnNavigationItemSelectedListener{
+
+
+    DrawerLayout drawer;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_turristea);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        setTitle("Turristea");
+        drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.addDrawerListener(toggle);
+        toggle.syncState();
+
+        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
+
+        Fragment f = new PrincipalFragment();
+        getSupportFragmentManager().beginTransaction().replace(R.id.contenedor,f).commit();
+    }
+
+
+    @Override
+    public void onBackPressed() {
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
+    }
+
+
+    @SuppressWarnings("StatementWithEmptyBody")
+    @Override
+    public boolean onNavigationItemSelected(MenuItem item) {
+        boolean fragmenTransaction = false;
+        Fragment f = null;
+        int id = item.getItemId();
+        if (id == R.id.nav_inicio) {
+            f = new PrincipalFragment();
+            fragmenTransaction = true;
+        } else if (id == R.id.nav_contenido) {
+
+        } else if (id == R.id.nav_info) {
+            f = new InformacionFragment();
+            fragmenTransaction = true;
+        } else if (id == R.id.nav_opinion) {
+            f=new OpinionFragment();
+            fragmenTransaction = true;
+        } else if (id == R.id.nav_registrar) {
+
+        } else if (id == R.id.nav_sesion) {
+            f = new IniciarSesionFragment();
+            fragmenTransaction = true;
+        }
+
+        if(fragmenTransaction){
+            getSupportFragmentManager().beginTransaction().replace(R.id.contenedor,f).commit();
+            item.setChecked(true);
+            getSupportActionBar().setTitle(item.getTitle());
+        }
+
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
+    }
+}
